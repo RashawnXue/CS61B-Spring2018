06d9fee0c84cd2c3f658492497c0c689b8c2e2aa
Rashawn <211250142@smail.nju.edu.cn>
2022-11-04 16:09:35 +0800

    complete lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,18 @@ public class LeapYear {
         }
     }
 
+    /** Judge whether a year is a leap year.
+     * @param year to be analyzed
+     * @return true if the year is a leap year and false if not
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 100 == 0) {
+            return year % 400 == 0;
+        } else {
+            return year % 4 == 0;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
